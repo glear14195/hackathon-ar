ae826dfec281d63f4bce6e62b72f403c91e29e55
akshat.sharma <akshat.sharma@squadrun.co>
2018-02-11 07:14:58 +0530

    Added some admin stuff

--- appStuff/admin.py
+++ appStuff/admin.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from appStuff.models import Advertisement, Advertiser
 # Register your models here.
+
+
+class AdvertiserAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request):
+        return False
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+admin.site.register(Advertiser, AdvertiserAdmin)
